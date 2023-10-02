de71794186c89950a9f6ff2ca0715af40256471f
Brady Salz <brady.salz@gmail.com>
2023-10-01 23:13:08 -0700

    drop handlebars for pug, setup generic table route
    Got way too tired of bashing my head on handlebars. Hit the ceiling when
    I couldn't figure out how to make the typescript importer happy. While
    it looked very nice and Jinja2-like (big fan of that templating engine)
    I decided it wasn't worth it.
    
    Did a couple Ctrl+F's and found Pug, looked super popular. Was
    significantly easier to get working. So much so that I can make an
    arbitrary "table viewer" rout, which makes my brain debug so much easier

--- app.ts
+++ app.ts
@@ -1,19 +1,17 @@
 import express from "express";
 import { insertCsvData, maybeCreateTables } from "./config/create";
 import { router } from "./src/routes";
-import { engine } from "express-handlebars";
 import { sequelize } from "./src/models";
 
 async function startServer() {
+    // Load some dummy data in
     await maybeCreateTables(sequelize).then(() =>
         insertCsvData("data/book.csv", "data/practices.csv", "data/songs.csv")
     );
 
     const app = express();
-
-    //Sets our app to use the handlebars engine
-    app.engine("hbs", engine({ extname: ".hbs" }));
-    app.set("view engine", ".hbs");
+    app.set("view engine", "pug");
+    app.set("views", "./views");
 
     app.use("/", router);
     app.listen(3000, () => {
--- src/routes.ts
+++ src/routes.ts
@@ -1,28 +1,54 @@
 import { Request, Response, Router } from "express";
-import { Book, Exercise, Practice, Section } from "./models";
+import {
+    Artist,
+    Book,
+    Exercise,
+    Practice,
+    Section,
+    Song,
+    User,
+} from "./models";
 
 export let router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-    res.render("home");
+    res.render("./layouts/index.pug");
 });
 
-router.get("/book", async (req: Request, res: Response) => {
-    const pracs = await Book.findAll({ raw: true });
-    res.render("./book_table", { book: pracs });
-});
-
-router.get("/section", async (req: Request, res: Response) => {
-    const pracs = await Section.findAll({ raw: true });
-    res.render("./section_table", { section: pracs });
-});
-
-router.get("/exercise", async (req: Request, res: Response) => {
-    const pracs = await Exercise.findAll({ raw: true });
-    res.render("./exercise_table", { exercises: pracs });
-});
-
-router.get("/practice", async (req: Request, res: Response) => {
-    const pracs = await Practice.findAll({ raw: true });
-    res.render("./table", { practices: pracs });
+/** Catches /table/<id>
+ * Useful mostly for debugging tables. It's nice to have this quick debug view
+ * to see what you're calling and working with.
+ */
+router.get("/table/:id", async (req: Request, res: Response) => {
+    let model: any;
+    switch (req.params.id) {
+        case "users":
+            model = User;
+            break;
+        case "books":
+            model = Book;
+            break;
+        case "exercises":
+            model = Exercise;
+            break;
+        case "practices":
+            model = Practice;
+            break;
+        case "sections":
+            model = Section;
+            break;
+        case "artists":
+            model = Artist;
+            break;
+        case "songs":
+            model = Song;
+            break;
+        default:
+            model = Practice;
+    }
+    // this weird line is needed because Tyepscript can't infer they're all
+    // compatible Model types for some reason
+    const data = await (model as any).findAll({ raw: true });
+    const fields = Object.keys(model.getAttributes());
+    res.render("./table", { fields: fields, rows: data });
 });
