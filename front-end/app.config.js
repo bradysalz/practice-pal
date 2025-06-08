972001c696ef23153bd178ba59a02f9a6994ad8b
Brady Salz <brady.salz@gmail.com>
2025-06-08 14:45:49 -0700

    app file changes for expo

--- front-end/app.config.js
+++ front-end/app.config.js
@@ -1,12 +1,49 @@
 import 'dotenv/config';
-import baseConfig from './app.json';
 
-export default ({ config }) => ({
-  ...baseConfig.expo,
-  name: 'PracticePal', // you can override values here if needed
-  slug: 'practice-pal',
+export default () => ({
+  name: 'PracticePal',
+  owner: 'bradysalz',
+  slug: 'practicepal',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'practicepal',
+  userInterfaceStyle: 'automatic',
+  jsEngine: 'hermes',
+  newArchEnabled: true,
+  ios: {
+    bundleIdentifier: 'com.bradysalz.practicepal',
+    supportsTablet: true
+  },
+  android: {
+    package: 'com.bradysalz.practicepal',
+    versionCode: 1,
+    adaptiveIcon: {
+      backgroundColor: '#ffffff'
+    },
+    edgeToEdgeEnabled: true
+  },
+  web: {
+    bundler: 'metro',
+    favicon: './assets/images/favicon.png'
+  },
+  plugins: [
+    'expo-router',
+    'expo-font',
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/images/splash-icon.png',
+        imageWidth: 200,
+        resizeMode: 'contain',
+        backgroundColor: '#ffffff'
+      }
+    ]
+  ],
+  experiments: {
+    typedRoutes: true
+  },
   extra: {
-    ...baseConfig.expo.extra,
     supabaseUrl: process.env.SUPABASE_URL,
     supabaseKey: process.env.SUPABASE_ANON_KEY,
   },
