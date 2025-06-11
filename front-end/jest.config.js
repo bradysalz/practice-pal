2f125f5361b22a957ea608f6882d8c5d54cccaba
Brady Salz <brady.salz@gmail.com>
2025-06-11 09:41:29 -0700

    add coverage reports

--- front-end/jest.config.js
+++ front-end/jest.config.js
@@ -9,4 +9,13 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/$1',
     '^uuid$': require.resolve('uuid'),
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    'lib/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  coverageReporters: ['text', 'lcov'],
 };
